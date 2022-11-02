e5c5592a9fd1657bb6b54eb0ad7cbefd1a0c2843
saif <saif@kali.kali1337>
2022-11-02 12:40:41 -0400

    Mise a jour DEPARTEMENT

---
+++ Majdepartement/MajDepartement.aspx
@@ -0,0 +1,94 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MajDepartement.aspx.cs" Inherits="Majdepartement.MajDepartement" %>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+<title></title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"/>
+
+
+
+</head>
+<body>
+<form id="form1" runat="server">
+<div>
+
+<div class="form-group">
+<label class="control-label col-sm-2">Numéro Département: </label><asp:TextBox ID="txtNoDepartmenet" min="1" class="form-control" width="20%" runat="server" TextMode="Number" ></asp:TextBox>
+<asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ControlToValidate="txtNoDepartmenet" EnableClientScript="False" ErrorMessage="Veuillez Saisir Numéro De Département"></asp:RequiredFieldValidator>
+</div>
+
+<div class="form-group">
+<label class="control-label col-sm-2">Nom Département: </label><asp:TextBox ID="txtNomDept" min="1" class="form-control" width="20%" runat="server"></asp:TextBox>
+<asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ControlToValidate="txtNomDept" EnableClientScript="False" ErrorMessage="Veuillez Saisir Le Nom Du Département"></asp:RequiredFieldValidator>
+</div>
+<div class="form-group">
+<label class="control-label col-sm-2">Location Département: </label><asp:TextBox ID="txtLocation" min="1" class="form-control" width="20%" runat="server"></asp:TextBox>
+<asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ControlToValidate="txtLocation" EnableClientScript="False" ErrorMessage="Veuillez Saisir La localité Du Département"></asp:RequiredFieldValidator>
+</div>
+
+<div class="form-group" style="text-align: center; width: 100%">
+<asp:Button class="btn btn-primary" Width="19%" ID="btnFirst" runat="server" Text="|<" OnClick="btnFirst_Click" />
+
+
+
+<asp:Button class="btn btn-primary" Width="19%" ID="btnPrevous" runat="server" Text="<" OnClick="btnPrevous_Click" />
+
+
+
+<asp:Button class="btn btn-primary" Width="19%" ID="btnNext" runat="server" Text=">" OnClick="btnNext_Click" />
+
+
+
+<asp:Button class="btn btn-primary" Width="19%" ID="btnLast" runat="server" Text=">|" OnClick="btnLast_Click" />
+
+
+
+</div>
+
+<div style="text-align: center; width: 100%">
+
+
+
+
+
+<asp:Button Width="22%" class="btn btn-success" ID="btnAdd" runat="server" Text="Ajouter" OnClick="btnAdd_Click" />
+
+
+
+
+
+<asp:Button Width="22%" class="btn btn-success" ID="btnModifier" runat="server" Text="Modifier" OnClick="btnModifier_Click" />
+
+
+
+
+
+<asp:Button Width="22%" class="btn btn-success" ID="btnSupprimer" runat="server" Text="Supprimer" OnClick="btnSupprimer_Click" />
+
+
+
+
+
+<asp:Button Width="22%" class="btn btn-success" ID="btnEnregistrer" runat="server" Text="Enregistrer" OnClick="btnEnregistrer_Click" />
+
+
+
+
+
+
+</div>
+<asp:GridView ID="gridDepartement" style="margin: 15px;" runat="server" class="table table-striped" AllowPaging="True" AllowSorting="True" PageSize="3" OnSorting="gridDepartement_Sorting" OnPageIndexChanging="gridDepartement_PageIndexChanging" OnSelectedIndexChanged="gridDepartement_SelectedIndexChanged" >
+<Columns>
+<asp:CommandField ShowSelectButton="True" />
+</Columns>
+</asp:GridView>
+
+
+
+
+</div>
+</form>
+
+</body>
+</html>
---
+++ Majdepartement/MajDepartement.aspx.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Majdepartement
+{
+    public partial class MajDepartement : System.Web.UI.Page
+    {
+
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-6003F0OI\SQLEXPRESS01;Initial Catalog=fadilizakaria;Integrated Security=True");
+        DataTable d = new DataTable();
+        static int index;
+        static SqlDataAdapter dataadapter;
+        static DataSet ds;
+        SqlCommandBuilder builder;
+        static SortedList<string, string> list = new SortedList<string, string>();
+
+        public void remplirdata()
+        {
+            ds = new DataSet();
+            d = new DataTable();
+            dataadapter = new SqlDataAdapter("select deptno as [Numero departement] ,ndep as [Nom departement] ,location as [Location Département] from dept ", con);
+            dataadapter.Fill(ds, "tabledept");
+            d = ds.Tables["tabledept"];
+            gridDepartement.DataSource = d;
+            gridDepartement.DataBind();
+            if(d.Rows.Count>0)
+            {
+                naviguer(0);
+            }
+        }
+        private void naviguer(int index)
+        {
+            txtNoDepartmenet.Enabled = false;
+            txtNomDept.Enabled = false;
+            txtLocation.Enabled = false;
+          txtNoDepartmenet.Text = d.Rows[index][0].ToString();
+            txtNomDept.Text = d.Rows[index][1].ToString();
+            txtLocation.Text = d.Rows[2].ToString();
+
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                remplirdata();
+                if (list.Count == 0)
+                {
+                    list.Add("Numero departement", "Desc");
+                    list.Add("Nom departement", "Desc");
+                    list.Add("Location", "Desc");
+                }
+            }
+        }
+        protected void btnFirst_Click(object sender, EventArgs e)
+        {
+            if(d.Rows.Count>0)
+            {
+                index = 0;
+                naviguer(index);
+            }
+        }
+
+        protected void btnPrevous_Click(object sender, EventArgs e)
+        {
+           
+            if(d.Rows.Count>0)
+            {
+                if(index>0)
+                {
+                    index--;
+                }
+                else
+                {
+                    index = 0;
+                }
+                naviguer(index);
+            }
+
+        }
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+            if(d.Rows.Count > 0)
+            {
+                if (index < d.Rows.Count - 1)
+                    index++;
+                naviguer(index);
+            }
+
+        }
+
+        protected void btnLast_Click(object sender, EventArgs e)
+        {
+            if(d.Rows.Count>0)
+            {
+                index = d.Rows.Count - 1;
+                naviguer(index);
+            }
+        }
+         private int gridview(int deptno)
+        {
+            foreach(DataRow r in d.Rows)
+            {
+                if (int.Parse(r[0].ToString())==deptno)
+                {
+                    return d.Rows.IndexOf(r);
+                }
+            }
+            return -1;
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if(btnAdd.Text.Equals("Ajouter"))
+            {
+                txtNoDepartmenet.Text = "";
+                txtNomDept.Text = "";
+                txtLocation.Text = "";
+                txtNoDepartmenet.Enabled = true;
+                txtNomDept.Enabled = true;
+                txtLocation.Enabled = true;
+                btnAdd.Text = "valider";
+            }
+            else
+            {
+                Page.Validate();
+                if(Page.IsValid)
+                {
+                    if(gridview(int.Parse(txtNoDepartmenet.Text.ToString()))==-1)
+                        {
+                        d.Rows.Add(int.Parse(txtNoDepartmenet.Text), txtNomDept.Text, txtLocation.Text);
+                        btnAdd.Text = "Ajouter";
+                        txtNoDepartmenet.Text = "";
+                        txtNomDept.Text = "";
+                        txtLocation.Text = "";
+                        naviguer(0);
+                        gridDepartement.DataSource = d;
+                        gridDepartement.DataBind();
+                    }
+                    else
+                    {
+                        Response.Write("le numero est déja existe");
+                        btnAdd.Text = "Ajouter";
+                        naviguer(0);
+                    }
+
+
+                }
+            }
+        }
+
+        protected void btnModifier_Click(object sender, EventArgs e)
+        {
+            if(btnModifier.Text.Equals("Modifier"))
+            {
+                txtNoDepartmenet.Enabled = false;
+                txtNomDept.Enabled = true;
+                txtLocation.Enabled = true;
+                btnModifier.Text = "valider";
+            }
+            else
+            {
+                Page.Validate();
+                if(Page.IsValid)
+                {
+                    d.Rows[index][1] = txtNomDept.Text;
+                    d.Rows[index][2] = txtLocation.Text;
+                    btnModifier.Text = "Modifier";
+                    gridDepartement.DataSource = d;
+                    gridDepartement.DataBind();
+                    naviguer(0);
+
+                }
+
+            }
+
+        }
+
+        protected void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            if(btnSupprimer.Text.Equals("Supprimer"))
+            {
+                txtNoDepartmenet.Enabled = false;
+                txtNomDept.Enabled = false;
+                txtLocation.Enabled = false;
+                btnSupprimer.Text = "valider";
+
+            }else
+
+            {
+                Page.Validate();
+                if(Page.IsValid)
+                {
+                    d.Rows[index].Delete();
+                    btnSupprimer.Text = "Supprimer";
+                    gridDepartement.DataSource = d;
+                    gridDepartement.DataBind();
+
+                    naviguer(0);
+                }
+            }
+
+        }
+
+        protected void btnEnregistrer_Click(object sender, EventArgs e)
+        {
+            builder = new SqlCommandBuilder(dataadapter);
+            dataadapter.Update(d);
+            remplirdata();
+            gridDepartement.DataSource = d;
+
+        }
+
+        protected void gridDepartement_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            GridViewRow row = gridDepartement.SelectedRow;
+            index = gridview(int.Parse(row.Cells[1].Text));
+            naviguer(index);
+        }
+
+        protected void gridDepartement_PageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            gridDepartement.PageIndex = e.NewPageIndex;
+            gridDepartement.DataSource = d;
+            gridDepartement.DataBind();
+
+        }
+
+        protected void gridDepartement_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            if (list[e.SortExpression].ToString().Trim().Equals("Asc"))
+                list[e.SortExpression] = "Desc";
+            else
+                list[e.SortExpression] = "Asc";
+            DataView view = new DataView(d);
+            view.Sort = e.SortExpression + " " + list[e.SortExpression].ToString();
+            gridDepartement.DataSource = view;
+            gridDepartement.DataBind();
+        }
+    }
+}
